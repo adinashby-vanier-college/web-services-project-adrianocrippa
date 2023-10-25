f73aefc274592b58047812fbaf00150b55f00276
Diana Majolli Andre <dianamajolli@hotmail.com>
2023-10-25 18:34:20 -0400

    Added data manipulation - check healthy ingredient

--- public/js/script_n.js
+++ public/js/script_n.js
@@ -1,4 +1,3 @@
-
 const titleInput = document.getElementById("title");
 const recipeInput = document.getElementById("recipe");
 const output = document.getElementById("output");
@@ -9,52 +8,68 @@ const txtSearch = document.querySelector("#txtSearch");
 const nutritionContainer = document.querySelector("#nutrition-container");
 const loadingEle = document.querySelector("#loading");
 
-const toggleLoad = (element, isShow) =>{
-    element.classList.toggle("hide", isShow);
+const toggleLoad = (element, isShow) => {
+  element.classList.toggle("hide", isShow);
 };
 
-function fetchNutrition (){
-    //let title = titleInput.value;
-    let ingr = txtSearch.value.split("\n");
-    
-    return fetch(`https://api.edamam.com/api/nutrition-details?app_id=${appID}&app_key=${apiKey}`, {
-        method: "POST",
-        cache: "no-cache",
-        headers:{
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ingr})
-        //body: JSON.stringify({title, ingr})
+function fetchNutrition() {
+  //let title = titleInput.value;
+  let ingr = txtSearch.value.split("\n");
+
+  return fetch(
+    `https://api.edamam.com/api/nutrition-details?app_id=${appID}&app_key=${apiKey}`,
+    {
+      method: "POST",
+      cache: "no-cache",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ingr }),
+      //body: JSON.stringify({title, ingr})
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      renderNutrion(data);
+      toggleLoad(loadingEle, true);
     })
-    .then(response => response.json())
-    .then ((data) => {renderNutrion(data); toggleLoad(loadingEle, true); })
-    .catch((error) => toggleLoad(loadingEle, false))
-    //.finally(() => setScrollPosition());
+    .catch((error) => toggleLoad(loadingEle, false));
+  //.finally(() => setScrollPosition());
 }
 
-btnFind.addEventListener("click", ()=>fetchNutrition());
-
+btnFind.addEventListener("click", () => fetchNutrition());
+txtSearch.addEventListener("keyup", (e) => {
+    
+    if(e.keyCode ===13){
+        fetchNutrition();
+    }
+} );
 
 const renderNutrion = (data = []) => {
-    nutritionContainer.innerHTML ="";
-    let fragments =[];
+  nutritionContainer.innerHTML = "";
+  let fragments = [];
 
-        Object.keys(data.totalDaily).forEach(key => {
-            let obj = data.totalDaily[key];
+  Object.keys(data.totalDaily).forEach((key) => {
+    let obj = data.totalDaily[key];
 
-            fragments.push(`<b>${obj.label}</b>${"        "}${parseFloat (obj.quantity).toFixed(1)}${obj.unit}<br>`);
-        });
-            
-            let html = `
-             <b>CALORIES</b> ${data.calories}<br>${fragments.join('')}
+    fragments.push(
+      `<b>${obj.label}</b>${"        "}${parseFloat(obj.quantity).toFixed(1)}${
+        obj.unit
+      }<br>`
+    );
+  });
+
+  let calorieMessage = "";
+
+  if (data.calories > 1000) {
+    calorieMessage= `<b style="color:red; font-size:30px">Attention</b> ingredient with high caloric value, not healthy <br>`;
+  } else {
+    calorieMessage= `<b style="color:green; font-size:30px">Healthy </b>ingredient <br>`;
+  }
+
+  let html = `
+          ${calorieMessage}<b>CALORIES</b> ${data.calories}<br>  ${fragments.join("")}
             `;
 
-    nutritionContainer.innerHTML = html;
- 
+  nutritionContainer.innerHTML = html;
 };
-
-
-
-
-
-
